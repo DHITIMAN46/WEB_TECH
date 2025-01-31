9672b026cc7b6a78b144c7a943846852b7854681
DHITIMAN46 <gogoiguduman@gmail.com>
2025-01-31 13:16:45 +0530

    Add files via upload

---
+++ assigment_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Assigment</title>
+</head>
+<body>
+    <?php 
+    $name = "Dhitiman";
+    ?>
+
+    <h1> Name is <?php echo $name;?> <h1>
+</body>
+</html>
